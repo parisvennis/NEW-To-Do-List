ae32c2e1ef190b80cb7c20a4f85db481cc89e61b
parisvennis <parisvennis@gmail.com>
2020-10-14 22:25:02 +0000

    todo

---
+++ src/js/component/ToDoList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class ToDoList extends React.Component {
+    render(){
+        return <div>"Hello World"</div>
+    }
+
+}
+
+export {ToDoList as default}
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,6 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import ToDoList from "./component/ToDoList";
 
-//include bootstrap npm library into the bundle
-import "bootstrap";
-
-//include your index.scss file into the bundle
-import "../styles/index.scss";
-
-//import your own components
-import { Home } from "./component/home.js";
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<ToDoList />, document.getElementById("app"))
